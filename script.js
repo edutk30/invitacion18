c97d500722fadd748f0cbf17a4536901f2a27d89
edutk30 <edutk918@gmail.com>
2024-12-06 14:56:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Agregar interactividad al botón de confirmar asistencia
+document.getElementById("confirmar").addEventListener("click", () => {
+    alert("¡Gracias por confirmar tu asistencia! 🎉 Nos vemos en el cumpleaños.");
+});
